691d4c9540d8994c69323cf6c2192f5d65d14490
Sandeep <sandeep.mane@gslab.com>
2017-11-01 14:52:46 +0530

    changes in sdk

---
+++ app/src/main/java/test/submodule/Status.java
@@ -0,0 +1,14 @@
+package test.submodule;
+
+/**
+ * Created by sandeep on 12/10/17.
+ */
+
+public class Status {
+
+    public String displayMessage() {
+        return "Hello SubModule";
+    }
+
+
+}
